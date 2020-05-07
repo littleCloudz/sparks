864d24f6abe9783205d6e9b8273538ff39784828
zhangxy <zhangxy@ctrip.com>
2020-05-07 22:32:11 +0800

    构建Vue组件（模拟a标签）以及相关知识点整理

--- 20200507/atag.js
+++ 20200507/atag.js
@@ -1,4 +1,34 @@
+Vue.component('a-tag', {
+    template: '<span @click="redirect" :class="tagClassName">abc</span>',
+    data(){
+      return {
+          tagClassName: 'atag'
+      }
+    },
+    props: {
+        href: {
+            type: String,
+            required: true
+        },
+        target: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        redirect(){
+            window.open(this.href, this.target)
+        }
+    }
+})
+
 new Vue({
     el: '#app',
 
+
 })
+
+
+
+
+
